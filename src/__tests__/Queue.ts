b75614eb7a7aa5e7b0d91c1e16aadde1ca7e94b0
Josef Kříž <pepakriz@gmail.com>
2018-11-15 20:10:14 +0100

    Queue: fixed hasJob while job is processing (#7)

--- src/__tests__/Queue.ts
+++ src/__tests__/Queue.ts
@@ -1,4 +1,5 @@
 import { Queue } from '../Queue';
+import { sleep } from '../Utils';
 
 it('runs two jobs', async () => {
 	const job = jest.fn();
@@ -23,4 +24,12 @@ it('runs again after done', async () => {
 	expect(job.mock.calls.length).toBe(2);
 	await queue.runJob('barJob', job);
 	expect(job.mock.calls.length).toBe(3);
+});
+
+it('hasJob while processing', async () => {
+	const queue = new Queue();
+	queue.runJob('fooJob', () => sleep(20));
+	await sleep(10);
+
+	expect(queue.hasJob('fooJob')).toBe(true);
 });
