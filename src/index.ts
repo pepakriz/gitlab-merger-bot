31c325efef976e42da944466fb2afcefd96bc6d3
Josef Kříž <pepakriz@gmail.com>
2019-09-08 21:42:19 +0200

    Code cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,12 @@ import * as env from 'env-var';
 import { assignToAuthorAndResetLabels } from './AssignToAuthor';
 import { setBotLabels } from './BotLabelsSetter';
 import { DiscussionNote, GitlabApi, MergeRequest, MergeRequestDiscussion, MergeStatus, User } from './GitlabApi';
-import { acceptMergeRequest, AcceptMergeRequestResultKind, BotLabels } from './MergeRequestAcceptor';
+import {
+	acceptMergeRequest,
+	AcceptMergeRequestResult,
+	AcceptMergeRequestResultKind,
+	BotLabels,
+} from './MergeRequestAcceptor';
 import { tryCancelPipeline } from './PipelineCanceller';
 import { sendNote } from './SendNote';
 import { Worker } from './Worker';
@@ -21,6 +26,94 @@ const SQUASH_MERGE_REQUEST = env.get('SQUASH_MERGE_REQUEST', 'true').asBoolStric
 
 const gitlabApi = new GitlabApi(GITLAB_URL, GITLAB_AUTH_TOKEN);
 const worker = new Worker();
+
+const resolveMergeRequestResult = async (result: AcceptMergeRequestResult) => {
+	const mergeRequestInfo = result.mergeRequestInfo;
+	const user = result.user;
+
+	if (result.kind === AcceptMergeRequestResultKind.SuccessfullyMerged) {
+		console.log(`[MR] Merge request is merged, ending`);
+		await setBotLabels(gitlabApi, mergeRequestInfo, []);
+		return;
+	}
+
+	if (result.kind === AcceptMergeRequestResultKind.CanNotBeMerged) {
+		let message = 'Merge request can\'t be merged';
+		const errorMessage = mergeRequestInfo.merge_error;
+		if (mergeRequestInfo.work_in_progress) {
+			message += `: MR is marked as WIP`;
+		} else if (errorMessage !== null) {
+			message += `: ${errorMessage}`;
+		}
+
+		console.log(`[MR] merge failed: ${message}, assigning back`);
+
+		await Promise.all([
+			assignToAuthorAndResetLabels(gitlabApi, mergeRequestInfo),
+			tryCancelPipeline(gitlabApi, mergeRequestInfo, user),
+			sendNote(gitlabApi, mergeRequestInfo, message),
+		]);
+
+		return;
+	}
+
+	if (result.kind === AcceptMergeRequestResultKind.ClosedMergeRequest) {
+		console.log(`[MR] Merge request is closed, ending`);
+
+		await Promise.all([
+			tryCancelPipeline(gitlabApi, mergeRequestInfo, user),
+			setBotLabels(gitlabApi, mergeRequestInfo, []),
+		]);
+
+		return;
+	}
+
+	if (result.kind === AcceptMergeRequestResultKind.ReassignedMergeRequest) {
+		console.log(`[MR] Merge request is assigned to different user, ending`);
+
+		await Promise.all([
+			tryCancelPipeline(gitlabApi, mergeRequestInfo, user),
+			setBotLabels(gitlabApi, mergeRequestInfo, []),
+		]);
+
+		return;
+	}
+
+	if (result.kind === AcceptMergeRequestResultKind.FailedPipeline) {
+		console.log(`[MR] pipeline is in failed state: ${result.pipeline.status}, assigning back`);
+
+		await Promise.all([
+			assignToAuthorAndResetLabels(gitlabApi, mergeRequestInfo),
+			sendNote(gitlabApi, mergeRequestInfo, `Merge request can't be merged due to failing pipeline`),
+		]);
+
+		return;
+	}
+
+	if (result.kind === AcceptMergeRequestResultKind.InvalidPipeline) {
+		const message = result.pipeline === null
+			? `Merge request can't be merged. Pipeline does not exist`
+			: `Merge request can't be merged. The latest pipeline is not executed on the latest commit`;
+
+		await Promise.all([
+			assignToAuthorAndResetLabels(gitlabApi, mergeRequestInfo),
+			sendNote(gitlabApi, mergeRequestInfo, message),
+		]);
+
+		return;
+	}
+
+	if (result.kind === AcceptMergeRequestResultKind.Unauthorized) {
+		console.log(`[MR] You don't have permissions to accept this merge request, assigning back`);
+
+		await Promise.all([
+			assignToAuthorAndResetLabels(gitlabApi, mergeRequestInfo),
+			sendNote(gitlabApi, mergeRequestInfo, `Merge request can't be merged due to insufficient authorization`),
+		]);
+
+		return;
+	}
+};
 
 const runMergeRequestCheckerLoop = async (user: User) => {
 	console.log('[bot] Checking assigned merge requests');
@@ -64,115 +157,25 @@ const runMergeRequestCheckerLoop = async (user: User) => {
 			return;
 		}
 
-		return mergeRequest;
+		const jobId = `accept-merge-${mergeRequest.id}`;
+		if (worker.hasJobInQueue(mergeRequest.target_project_id, jobId)) {
+			return;
+		}
+
+		worker.addJobToQueue(
+			mergeRequest.target_project_id,
+			jobId,
+			() => acceptMergeRequest(gitlabApi, mergeRequest, user, {
+				ciInterval: CI_CHECK_INTERVAL,
+				removeBranchAfterMerge: REMOVE_BRANCH_AFTER_MERGE,
+				squashMergeRequest: SQUASH_MERGE_REQUEST,
+			}),
+		).then(resolveMergeRequestResult);
+
+		await setBotLabels(gitlabApi, mergeRequest, [BotLabels.InMergeQueue]);
 	});
 
-	(await Promise.all(possibleToAcceptMergeRequests))
-		.map(async (mergeRequest?: MergeRequest) => {
-			if (mergeRequest === undefined) {
-				return;
-			}
-
-			const jobId = `accept-merge-${mergeRequest.id}`;
-			if (worker.hasJobInQueue(mergeRequest.target_project_id, jobId)) {
-				return;
-			}
-
-			await setBotLabels(gitlabApi, mergeRequest, [BotLabels.InMergeQueue]);
-
-			const result = await worker.addJobToQueue(
-				mergeRequest.target_project_id,
-				jobId,
-				() => acceptMergeRequest(gitlabApi, mergeRequest, user, {
-					ciInterval: CI_CHECK_INTERVAL,
-					removeBranchAfterMerge: REMOVE_BRANCH_AFTER_MERGE,
-					squashMergeRequest: SQUASH_MERGE_REQUEST,
-				}),
-			);
-
-			if (result.kind === AcceptMergeRequestResultKind.SuccessfullyMerged) {
-				console.log(`[MR] Merge request is merged, ending`);
-				await setBotLabels(gitlabApi, result.mergeRequestInfo, []);
-				return;
-			}
-
-			if (result.kind === AcceptMergeRequestResultKind.CanNotBeMerged) {
-				let message = 'Merge request can\'t be merged';
-				const errorMessage = result.mergeRequestInfo.merge_error;
-				if (result.mergeRequestInfo.work_in_progress) {
-					message += `: MR is marked as WIP`;
-				} else if (errorMessage !== null) {
-					message += `: ${errorMessage}`;
-				}
-
-				console.log(`[MR] merge failed: ${message}, assigning back`);
-
-				await Promise.all([
-					assignToAuthorAndResetLabels(gitlabApi, result.mergeRequestInfo),
-					tryCancelPipeline(gitlabApi, result.mergeRequestInfo, user),
-					sendNote(gitlabApi, mergeRequest, message),
-				]);
-
-				return;
-			}
-
-			if (result.kind === AcceptMergeRequestResultKind.ClosedMergeRequest) {
-				console.log(`[MR] Merge request is closed, ending`);
-
-				await Promise.all([
-					tryCancelPipeline(gitlabApi, result.mergeRequestInfo, user),
-					setBotLabels(gitlabApi, result.mergeRequestInfo, []),
-				]);
-
-				return;
-			}
-
-			if (result.kind === AcceptMergeRequestResultKind.ReassignedMergeRequest) {
-				console.log(`[MR] Merge request is assigned to different user, ending`);
-
-				await Promise.all([
-					tryCancelPipeline(gitlabApi, result.mergeRequestInfo, user),
-					setBotLabels(gitlabApi, result.mergeRequestInfo, []),
-				]);
-
-				return;
-			}
-
-			if (result.kind === AcceptMergeRequestResultKind.FailedPipeline) {
-				console.log(`[MR] pipeline is in failed state: ${result.pipeline.status}, assigning back`);
-
-				await Promise.all([
-					assignToAuthorAndResetLabels(gitlabApi, result.mergeRequestInfo),
-					sendNote(gitlabApi, mergeRequest, `Merge request can't be merged due to failing pipeline`),
-				]);
-
-				return;
-			}
-
-			if (result.kind === AcceptMergeRequestResultKind.InvalidPipeline) {
-				const message = result.pipeline === null
-					? `Merge request can't be merged. Pipeline does not exist`
-					: `Merge request can't be merged. The latest pipeline is not executed on the latest commit`;
-
-				await Promise.all([
-					assignToAuthorAndResetLabels(gitlabApi, result.mergeRequestInfo),
-					sendNote(gitlabApi, mergeRequest, message),
-				]);
-
-				return;
-			}
-
-			if (result.kind === AcceptMergeRequestResultKind.Unauthorized) {
-				console.log(`[MR] You don't have permissions to accept this merge request, assigning back`);
-
-				await Promise.all([
-					assignToAuthorAndResetLabels(gitlabApi, result.mergeRequestInfo),
-					sendNote(gitlabApi, mergeRequest, `Merge request can't be merged due to insufficient authorization`),
-				]);
-
-				return;
-			}
-		});
+	await Promise.all(possibleToAcceptMergeRequests);
 
 	setTimeout(() => runMergeRequestCheckerLoop(user), MR_CHECK_INTERVAL);
 };
