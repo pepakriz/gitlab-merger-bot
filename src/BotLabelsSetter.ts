2ab268e2c995514f53e1ed1fdbfdc08729a451e1
Josef Kříž <pepakriz@gmail.com>
2020-03-24 19:39:45 +0100

    Initial gh-pages commit

--- src/BotLabelsSetter.ts
+++
@@ -1,8 +0,0 @@
-import { GitlabApi, MergeRequest } from './GitlabApi';
-import { BotLabels, filterBotLabels } from './MergeRequestAcceptor';
-
-export const setBotLabels = async (gitlabApi: GitlabApi, mergeRequest: MergeRequest, labels: BotLabels[]) => {
-	await gitlabApi.updateMergeRequest(mergeRequest.project_id, mergeRequest.iid, {
-		labels: [...filterBotLabels(mergeRequest.labels), ...labels].join(','),
-	});
-};
--- src/SendNote.ts
+++
@@ -1,4 +0,0 @@
-import { GitlabApi, MergeRequest } from './GitlabApi';
-
-export const sendNote = (gitlabApi: GitlabApi, mergeRequest: MergeRequest, body: string): Promise<void> =>
-	gitlabApi.createMergeRequestNote(mergeRequest.project_id, mergeRequest.iid, body);
