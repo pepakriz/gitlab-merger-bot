4cda7de0a2a6828d79470a8304f34f8956ebbbca
Josef Kříž <pepakriz@gmail.com>
2024-03-24 10:04:36 +0100

    fix for Material UI menu color

--- dashboard/src/components/layout.tsx
+++ dashboard/src/components/layout.tsx
@@ -3,13 +3,9 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import React from 'react';
-import Link from 'next/link';
-import { Button } from '@mui/material';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
 import { useQuery } from '@apollo/client';
 import { MeQuery } from '../types';
 import gql from 'graphql-tag';
@@ -32,21 +28,9 @@ export const Layout = ({ children }: LayoutProps) => {
 		<>
 			<AppBar position='relative'>
 				<Toolbar>
-					<Box mr={2}>
-						<Link href={'/'} passHref>
-							<Button color='inherit'>
-								<Avatar alt='Remy Sharp' src={data?.me.avatarUrl} />
-								&nbsp;&nbsp;&nbsp;
-								<Typography variant='h6'>{data?.me.name}</Typography>
-							</Button>
-						</Link>
-					</Box>
-
-					<Tabs value={0} aria-label='disabled tabs example'>
-						<Link href={'/'} passHref>
-							<Tab component={'a'} label='Queues' />
-						</Link>
-					</Tabs>
+					<Avatar alt='Remy Sharp' src={data?.me.avatarUrl} />
+					&nbsp;&nbsp;&nbsp;
+					<Typography variant='h6'>{data?.me.name}</Typography>
 				</Toolbar>
 			</AppBar>
 			<Container maxWidth={'lg'}>
